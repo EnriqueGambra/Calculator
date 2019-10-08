a48223afed8b5f19b3ba4ee9311e32d99866b202
Enrique Gambra <gambra7@gmail.com>
2019-10-07 22:47:01 -0400

    Added textfield for the screen. Hid caret.

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -1,29 +1,39 @@
+import java.awt.Color;
+import java.awt.Dimension;
 import javax.swing.JFrame;
 import javax.swing.JButton;
 import java.awt.FlowLayout;
 import javax.swing.JTextField;
+import javax.swing.JLabel;
 
 public class Calculator {
     static JFrame frame = new JFrame("Calculator");
-    static final int calcWidth = 500;
-    static final int calcHeight = 700;
+    static final int calcWidth = 400;
+    static final int calcHeight = 500;
     static JTextField calcScreen;
+    static JButton btnAdd;
     
     public static void main(String[] args) {
         createFrameProperties();
-        addCalculatorProperties();
+        addTextField();
+        frame.setVisible(true);
     }
     
     public static void createFrameProperties(){
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(calcWidth, calcHeight);
         frame.getContentPane().setLayout(new FlowLayout());
-        frame.setVisible(true);
     }
     
-    public static void addCalculatorProperties(){
-        calcScreen = new JTextField(" ", 10);
-        frame.getContentPane().add(calcScreen, 100);
+    public static void addTextField(){
+        calcScreen = new JTextField("");
+        calcScreen.setPreferredSize(new Dimension(calcWidth - 30, 75));
+        calcScreen.setCaretColor(Color.WHITE);
+        frame.getContentPane().add(calcScreen);
+    }
+    
+    public static void addButtons(){
+        
     }
     
 }
